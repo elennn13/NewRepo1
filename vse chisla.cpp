d9b9fe8e794750bc532edbe4b12abcf71644fb9f
elennn13 <154688999+elennn13@users.noreply.github.com>
2024-02-18 19:34:38 +0500

    Add files via upload

---
+++ vse chisla.cpp
@@ -0,0 +1,28 @@
+﻿//Вывести на экран все 
+// числа от нуля до введенного 
+// пользователем числа..cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+  setlocale(LC_ALL, "ru");
+    int n; 
+    int i = 0;
+    int num;
+
+    
+    cout << ("Введите число ");
+    cin >> n;
+    
+    for (int i = 0; i <= n; i++)
+    { 
+    cout << " ";
+    cout << i << endl;
+    }
+    return 0;
+}
